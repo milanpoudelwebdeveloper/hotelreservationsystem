12bee405110bbe632f60ff9b38606f65f9954862
Milan Poudel <milanwebdeveloper1@gmail.com>
2021-08-05 18:46:28 +0545

    Used chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,12 +5,14 @@ import (
 	"hotelsystem/pkg/handlers"
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module hotelsystem
 
 go 1.16
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+require github.com/go-chi/chi/v5 v5.0.3
